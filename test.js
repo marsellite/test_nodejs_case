2fe9f4225aebc0ed33dd405b6612b9c7420ff63d
marsellite <Minghao.Ni@latticesemi.com>
2016-06-13 19:06:09 +0800

    redirect everything to wechat public page

--- test.js
+++ test.js
@@ -1,4 +1,7 @@
 var express = require("express");
 var app = express();
-app.use(function(req, res){res.send("hellow");} );
+app.use(function(req, res){
+	res.redirect('http://mp.weixin.qq.com/mp/homepage?__biz=MzI3MzA5MzM3NQ==&hid=1&sn=58e9aba4f9c44a37eaa2ba022a4200a3&pass_ticket=SoG41GErn%2BoNA%2B3lHgfPnRV1ssnbhmwBGmIaXw1Tv7TfVUujp8uT2hKt7BdOqxKZ');
+	} 
+);
 app.listen("8080");
